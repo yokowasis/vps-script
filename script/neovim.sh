77cf4c21788ea70b051d63cd919c54d15afb1675
Your Name <you@example.com>
2026-02-04 20:59:05 +0800

    git pull

--- script/neovim.sh
+++ script/neovim.sh
@@ -5,5 +5,6 @@ sudo apt-get install -y ninja-build gettext cmake curl build-essential git
 git clone https://github.com/neovim/neovim.git
 cd neovim
 git checkout stable
+git pull
 make CMAKE_BUILD_TYPE=Release
 sudo make install
